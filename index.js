487e89ba7b0ace21c66d1bb95c45d22bf964b79b
Whorohit <ram21296@gmail.com>
2024-01-17 00:40:12 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
-const BASE_URL=process.env.BASE_URL
-const port = process.env.PORT|| 90;
+const BASE_URL = process.env.BASE_URL
+const port = process.env.PORT || 90;
 const app = express();
 app.use(express.urlencoded());
 app.use(express.json());
@@ -16,35 +16,62 @@ mongoose.connect('mongodb+srv://ram211296:root@cluster0.yqlv4lb.mongodb.net/?ret
 const bookSchema = mongoose.Schema({
     title: String,
     author: String,
-    year: Number,
+    year: {
+        type: String,
+        
+    },
     isbn: Number,
-    des:String,
+    desc: String,
 })
 const book = mongoose.model('book', bookSchema)
 
-app.post("/addbook", (req, res) => {
+app.post("/addbook", async (req, res) => {
     console.log(req.body)
-    const { title,author,year,isbn,desc } = req.body;
-    const bookobj = new book({
-        title,
-        author,
-        year,
-        isbn,
-        desc,
-    })
-    bookobj.save().then(() => {
-        console.log("send data")
+    const { title, author, year, isbn, desc } = req.body;
+    try {
+        const bookobj = new book({
+            title,
+            author,
+            year,
+            isbn,
+            desc,
+        })
+        console.log(bookobj);
+        const dta = await bookobj.save();
+        if (dta) {
+            
+            res.send({ message: "sucessfully added data" ,dta:dta })
+        }
+        else {
+            res.status(500).send({ message: "Internal Server Error" });
+        }
 
-    })
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
+    }
+
 })
 app.get("/", async (req, res) => {
-  
-    book.find({})
-        .then(data => {
-            console.log("Database Courses:")
-            res.send(data);
-        })
 
+try {
+    const data= await book.find({})
+    if(data)
+    {
+      res.send(data)
+    }
+    else
+    {
+        res.status(500).send("NO data");
+    }
+    
+
+    
+} catch (error) {
+    console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
+    
+}
 
 })
 app.get("/:id", async (req, res) => {
@@ -53,18 +80,18 @@ app.get("/:id", async (req, res) => {
         return res.status(400).send("ID parameter is required");
     }
     book.findById(id)
-    .then(data => {
-        if (!data) {
-            return res.status(404).send("Book not found");
-        }
+        .then(data => {
+            if (!data) {
+                return res.status(404).send("Book not found");
+            }
 
-        console.log("Found Book:", data);
-        res.send(data);
-    })
-    .catch(error => {
-        console.error("Error:", error);
-        res.status(500).send("Internal Server Error");
-    });
+            console.log("Found Book:", data);
+            res.send(data);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            res.status(500).send("Internal Server Error");
+        });
 })
 app.post("/delete", (req, res) => {
     const { id } = req.body
@@ -76,7 +103,7 @@ app.post("/delete", (req, res) => {
             })
     )
 })
-    app.listen(port, (req, res) => {
-        console.log(`the  server is running at ${port}`);
-    })
+app.listen(port, (req, res) => {
+    console.log(`the  server is running at ${port}`);
+})
 
